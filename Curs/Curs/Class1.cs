8bc0bd14d9b4571e3d6252599b0fb8872c23f0ef
QwertFil <48272472+QwertFil@users.noreply.github.com>
2019-03-06 10:11:20 +0300

    Скелет
    Скелет программы + форма.

---
+++ Curs/Curs/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Curs
+{
+    class Class1
+    {
+        static public string Operatia_XOR(string peremen1, string peremen3)
+        {
+            
+        }
+        static public string Operatia_OR(string peremen1, string peremen3)
+        {
+           
+        }
+        static public string Operatia_AND(string peremen1, string peremen3)
+        {
+          
+        }
+        static public string Operatia(string peremen1, string peremen2, string peremen3)
+        {
+         
+        }
+    }
+}
